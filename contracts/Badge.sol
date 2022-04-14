f2dea21b3d7c78f55a598cc56bac50d2beb66fea
Hendra Danu Laksana <hdl.project.co@gmail.com>
2022-04-14 23:38:49 +0700

    feat: add badge erc721 token

---
+++ contracts/Badge.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+
+contract Badge is NFTokenMetadata, Ownable {
+    constructor() {
+        nftName = "Badge NFT";
+        nftSymbol = "BDG";
+    }
+
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _uri);
+    }
+}
