a9e299623a8c5688fc628e5a131aa8c42b5b157a
Hendra Danu Laksana <hdl.project.co@gmail.com>
2022-04-29 23:50:43 +0700

    feat(badge): add access control

--- contracts/Badge.sol
+++ contracts/Badge.sol
@@ -1,17 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Badge is NFTokenMetadata, Ownable {
-    constructor() {
-        nftName = "Badge NFT";
-        nftSymbol = "BDG";
+contract Badge is ERC721URIStorage, AccessControl, Ownable {
+    using Address for address;
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
+
+    bytes32 public constant MINTER = keccak256("MINTER");
+
+    Counters.Counter private _tokenIds;
+
+    constructor ()
+    public
+    ERC721("badge", "BDG")
+    {}
+
+    function addMinter(address _minter)
+    public
+    onlyOwner
+    {
+        _grantRole(MINTER, _minter);
     }
 
-    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
-        super._mint(_to, _tokenId);
-        super._setTokenUri(_tokenId, _uri);
+    function removeMinter(address _minter)
+    public
+    onlyOwner
+    {
+        _revokeRole(MINTER, _minter);
+    }
+
+    function mint(address account, string memory tokenURI)
+    public
+    onlyRole(MINTER)
+    returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _safeMint(account, newTokenId);
+
+        _setTokenURI(newItemId, tokenURI);
+
+        return newTokenId;
     }
 }
